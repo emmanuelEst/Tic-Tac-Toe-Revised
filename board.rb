5f3748847b14f0f3c1dab27d21597d37ef006083
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2024-08-07 14:35:32 -0500

    Add game board visualizer
    To generate this board two methods are used
    
    The first method generate just one row of the board
    The second method will generate the entire board
    
    Escape code is used in both of these methods to make the console output appear cleaner
    "\e[" Marks the start of the sequence and \e[0m marks the end of the sequence

--- board.rb
+++ board.rb
@@ -7,4 +7,24 @@ class Board
   def initialize
     @states = Array.new(3) {Array.new(3,0)} # Create a 2-D Array where 0 is a space with no sign in place
   end
+
+  def generate_row(last_row = false)
+    # Creates game board row and then prints the row to the console
+    # Use with a method to generate the entire board
+    # '\e[' escape code in use
+    if last_row == true 
+      puts Array.new(5) { |i| i % 2 == 0 ? ' ' : '|' }.join('')
+    else
+      arr = Array.new(5) { |i| i % 2 == 0 ? "\e[4m \e[0m" : "\e[4m|\e[0m" } # when i is even '_' will be added | when i is not even '|' is added
+      puts arr.join('')
+    end
+  end
+
+  def generate_board
+    puts "\n"
+    puts "\e[4;1mGame Board:\e[0m"
+    puts "\n"
+    3.times {|i| i == 2 ? generate_row(true) : generate_row}
+    puts "\n"
+  end
 end
