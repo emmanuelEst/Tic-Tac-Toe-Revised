6e01c9cc89d4464a9850120b6568f5bb24ade7c8
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2024-08-10 00:28:22 -0500

    Modify board generation methods to be dynamic
    The previous version can only generate an 'empty' board
    
    With these changes, the board will properly display the 'X' and 'O'
    tokens when the players place them

--- board.rb
+++ board.rb
@@ -4,27 +4,43 @@
 class Board
   attr_accessor :states
 
+  BOARD_BUILDER = {
+    0 => ' ',
+    'X' => 'X',
+    'O' => 'O'
+  }.freeze
+
   def initialize
     @states = Array.new(3) { Array.new(3, 0) } # Create a 2-D Array where 0 is a space with no sign in place
   end
 
-  def generate_row(last_row: false)
+  def generate_row(states_array, row_number, last_row: false)
     # Creates game board row and then prints the row to the console
     # Use with a method to generate the entire board
     # '\e[' escape code in use
-    if last_row == true
-      puts Array.new(5) { |i| i.even? ? ' ' : '|' }.join('')
-    else
-      arr = Array.new(5) { |i| i.even? ? "\e[4m \e[0m" : "\e[4m|\e[0m" } # when i is even '_' will be added | when i is not even '|' is added
-      puts arr.join('')
-    end
+    arr = Array.new(5) do |index|
+      if index.even?
+        # Divide by 2 to get the right index in the states array
+        BOARD_BUILDER[states_array[row_number][index / 2]] 
+      else
+        '|'
+      end
+    end.join('')
+    # False will underline all the characters in the row | True will remove the underline
+    last_row ? (puts arr.insert(0, "\e[0m")) : (puts arr.insert(0, "\e[4m"))
   end
 
   def generate_board
     puts "\n"
     puts "\e[4;1mGame Board:\e[0m"
     puts "\n"
-    3.times { |i| i == 2 ? generate_row(last_row: true) : generate_row }
-    puts "\n"
+    # One the last row underline formatting will be removed
+    3.times do |i|
+      if i == 2
+        generate_row(@states, i, last_row: true) if i == 2
+      else
+        generate_row(@states, i)
+      end
+    end
   end
 end
